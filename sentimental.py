3e72aeda17253bd992f57996681cf0824eec1923
Zachary Bush <zmbush@berkeley.edu>
2012-03-16 19:10:18 -0700

    sentimental splits files

---
+++ sentimental.py
@@ -0,0 +1,22 @@
+import json
+
+data = 'yelp_academic_dataset.json'
+
+dataset = open(data)
+
+lines = 0
+
+types = {}
+for line in dataset:
+  reviewData = json.loads(line)
+  t = reviewData['type']
+  if t not in types:
+    types[t] = []
+  types[t].append(reviewData)
+
+for t in types:
+  fname = t + '.json'
+  handle = open(fname, 'w')
+  for line in types[t]:
+    handle.write(json.dumps(line))
+  handle.close()
